cf4d4d95bffc9b3cbf10c9f54b9c780e4349d4ed
AbhishekBaranwal2000 <abhishekbaranwal2000@gmail.com>
2023-06-05 16:34:56 +0530

    added Product package

--- src/main/java/com/project/ecommerce/products/entities/ProductCatogery.java
+++ src/main/java/com/project/ecommerce/products/entities/ProductCatogery.java
@@ -1,11 +1,13 @@
 package com.project.ecommerce.products.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class ProductCatogery {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
